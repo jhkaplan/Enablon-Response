f29586e1f990e75237eefe33c6ac2fc90763ee12
Josh Kaplan <kaplan.joshua@gmail.com>
2019-04-18 13:04:52 -0400

    added alert detail VC
    VC related to clicking on an alert item list item from list VC

---
+++ Enablon Response/Enablon Response/AlertDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlertDetailViewController.swift
+//  Enablon Response
+//
+//  Created by Josh Kaplan on 4/18/19.
+//  Copyright © 2019 Josh Kaplan. All rights reserved.
+//
+
+import UIKit
+
+class AlertDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
